f6fd2e2455c28673c2e2fec6f9a6769fc126453d
yinleon <hello.leonyin@gmail.com>
2018-03-06 19:47:45 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from setuptools import setup
+
+setup(name='runtimestamp',
+	packages=['runtimestamp'],
+	version='0.0.1',
+	description='Leaves an timestamp and signature at the top of every notebook',
+	author='leon yin',
+	author_email='ly501@nyu.edu',
+	url='https://github.com/yinleon/s3',
+	keywords='jupyter notebook, timestamp',
+	license='MIT',
+	install_requires=[
+		'platform',
+		'datetime',
+	]
+)a
