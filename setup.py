fcc42c871bc15c9830f46738bcb2e8f535c68994
Leon Yin <leonyin@172-16-29-73.DYNAPOOL.NYU.EDU>
2018-03-07 15:32:15 -0500

    all reqs are defaults

--- setup.py
+++ setup.py
@@ -3,15 +3,12 @@ from setuptools import setup
 
 setup(name='runtimestamp',
 	packages=['runtimestamp'],
-	version='0.0.1',
+	version='0.0.2',
 	description='Leaves an timestamp and signature at the top of every notebook',
 	author='leon yin',
 	author_email='ly501@nyu.edu',
 	url='https://github.com/yinleon/runtimestamp',
 	keywords='jupyter notebook, timestamp',
 	license='MIT',
-	install_requires=[
-		'platform',
-		'datetime',
-	]
+	install_requires=[]
 )
